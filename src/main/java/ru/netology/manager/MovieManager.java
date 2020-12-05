c543798777144cb19286b531b0e4c7abe897351a
Dmitry Urin <domainlover@yahoo.com>
2020-12-05 18:35:00 +0200

    The Code edited

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -3,43 +3,39 @@ package ru.netology.manager;
 import ru.netology.domain.MovieItem;
 
 public class MovieManager {
-  private MovieItem[] movies = new MovieItem[0];
+  private MovieItem[] items = new MovieItem[0];
   private int numberOfMovies = 10;
 
-  public void add(MovieItem movie) {
+  public void add(MovieItem item) {
     // создаём новый массив размером на единицу больше
-    int length = movies.length + 1;
+    int length = items.length + 1;
     MovieItem[] tmp = new MovieItem[length];
     // itar + tab
     // копируем поэлементно
     // for (int i = 0; i < items.length; i++) {
     //   tmp[i] = items[i];
     // }
-    System.arraycopy(movies, 0, tmp, 0, movies.length);
+    System.arraycopy(items, 0, tmp, 0, items.length);
     // кладём последним наш элемент
     int lastIndex = tmp.length - 1;
-    tmp[lastIndex] = movie;
-    movies = tmp;
+    tmp[lastIndex] = item;
+    items = tmp;
   }
 
-  public MovieItem[] showLastMovies() {
-    MovieItem[] result = new MovieItem[movies.length];
-    MovieItem[] numberOfMovies = new MovieItem[this.numberOfMovies];
+  public MovieItem[] showLastItems() {
+    MovieItem[] result = new MovieItem[Math.min(items.length, numberOfMovies)];
     for (int i = 0; i < result.length; i++) {
-      int index = movies.length - i - 1;
-      result[i] = movies[index];
+      int index = items.length - i - 1;
+      result[i] = items[index];
     }
-    for (int i = 0; i < this.numberOfMovies; i++) {
-      numberOfMovies[i] = result[i];
-    }
-    return numberOfMovies;
+    return result;
   }
 
   public MovieManager() {
   }
 
   public MovieManager(int numberOfMovies) {
-    if (numberOfMovies > 10) {
+    if (numberOfMovies < 0) {
       return;
     }
     this.numberOfMovies = numberOfMovies;
--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MovieManagerTest {
 
   @Test
-  public void showLessThanTenMovies() {
+  public void showLessThanTenItems1() {
     MovieManager movieManager = new MovieManager(5);
     MovieItem one = new MovieItem(1, 1, "one", "action");
     MovieItem two = new MovieItem(2, 2, "two", "cartoon");
@@ -20,14 +20,53 @@ public class MovieManagerTest {
     movieManager.add(three);
     movieManager.add(four);
     movieManager.add(five);
-    MovieItem[] actual = movieManager.showLastMovies();
+    MovieItem[] actual = movieManager.showLastItems();
     MovieItem[] expected = new MovieItem[]{five, four, three, two, one};
     assertArrayEquals(expected, actual);
   }
 
   @Test
-  public void showLastTenMovies() {
-    MovieManager movieManager = new MovieManager();
+  public void showLessThanTenItems2() {
+    MovieManager movieManager = new MovieManager(1);
+    MovieItem one = new MovieItem(1, 1, "one", "action");
+    movieManager.add(one);
+    MovieItem[] actual = movieManager.showLastItems();
+    MovieItem[] expected = new MovieItem[]{one};
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void showLessThanTenItems3() {
+    MovieManager movieManager = new MovieManager(9);
+    MovieItem one = new MovieItem(1, 1, "one", "action");
+    MovieItem two = new MovieItem(2, 2, "two", "cartoon");
+    MovieItem three = new MovieItem(3, 3, "three", "comedy");
+    MovieItem four = new MovieItem(4, 4, "four", "action");
+    MovieItem five = new MovieItem(5, 5, "five", "horror");
+    MovieItem six = new MovieItem(6, 6, "six", "cartoon");
+    MovieItem seven = new MovieItem(7, 7, "seven", "comedy");
+    MovieItem eight = new MovieItem(8, 8, "eight", "action");
+    MovieItem nine = new MovieItem(9, 9, "nine", "cartoon");
+    movieManager.add(one);
+    movieManager.add(two);
+    movieManager.add(three);
+    movieManager.add(four);
+    movieManager.add(five);
+    movieManager.add(six);
+    movieManager.add(seven);
+    movieManager.add(eight);
+    movieManager.add(nine);
+    MovieItem[] actual = movieManager.showLastItems();
+    MovieItem[] expected = new MovieItem[]{nine, eight, seven, six, five, four, three, two, one};
+    assertArrayEquals(expected, actual);
+  }
+
+
+
+
+  @Test
+  public void showLastTenItems() {
+    MovieManager movieManager = new MovieManager(10);
     MovieItem one = new MovieItem(1, 1, "one", "action");
     MovieItem two = new MovieItem(2, 2, "two", "cartoon");
     MovieItem three = new MovieItem(3, 3, "three", "comedy");
@@ -48,13 +87,13 @@ public class MovieManagerTest {
     movieManager.add(eight);
     movieManager.add(nine);
     movieManager.add(ten);
-    MovieItem[] actual = movieManager.showLastMovies();
+    MovieItem[] actual = movieManager.showLastItems();
     MovieItem[] expected = new MovieItem[]{ten, nine, eight, seven, six, five, four, three, two, one};
     assertArrayEquals(expected, actual);
   }
 
   @Test
-  public void showMoreThanTenMovies() {
+  public void showMoreThanTenItems() {
     MovieManager movieManager = new MovieManager();
     MovieItem one = new MovieItem(1, 1, "one", "action");
     MovieItem two = new MovieItem(2, 2, "two", "cartoon");
@@ -67,7 +106,6 @@ public class MovieManagerTest {
     MovieItem nine = new MovieItem(9, 9, "nine", "cartoon");
     MovieItem ten = new MovieItem(10, 10, "ten", "comedy");
     MovieItem eleven = new MovieItem(11, 11, "eleven", "action");
-    MovieItem twelve = new MovieItem(12, 12, "twelve", "horror");
     movieManager.add(one);
     movieManager.add(two);
     movieManager.add(three);
@@ -79,9 +117,8 @@ public class MovieManagerTest {
     movieManager.add(nine);
     movieManager.add(ten);
     movieManager.add(eleven);
-    movieManager.add(twelve);
-    MovieItem[] actual = movieManager.showLastMovies();
-    MovieItem[] expected = new MovieItem[] {twelve, eleven, ten, nine, eight, seven, six, five, four, three};
+    MovieItem[] actual = movieManager.showLastItems();
+    MovieItem[] expected = new MovieItem[] {eleven, ten, nine, eight, seven, six, five, four, three, two};
     assertArrayEquals(expected, actual);
   }
 }
